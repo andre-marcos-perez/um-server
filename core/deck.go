6e03ec7525ebcb552b68e419526cd6f6ddedb9ba
Andre Perez <andre.perez@sumup.com>
2025-08-25 14:52:17 +0200

    card and deck

--- core/deck.go
+++ core/deck.go
@@ -1,1 +1,46 @@
 package core
+
+import (
+	"errors"
+	"github.com/andre-marcos-perez/um-server/sequence"
+)
+
+var (
+	ErrDeckEmpty = errors.New("empty deck")
+)
+
+type Deck struct {
+	cards sequence.LinkedList[Card]
+}
+
+func NewDeck(options ...func(*Deck)) *Deck {
+	deck := &Deck{
+		cards: *sequence.NewLinkedList[Card](),
+	}
+	for _, option := range options {
+		option(deck)
+	}
+	return deck
+}
+
+func WithCards(cards []Card) func(*Deck) {
+	return func(deck *Deck) {
+		for _, card := range cards {
+			deck.cards.Insert(card)
+		}
+	}
+}
+
+func WithShuffle() func(*Deck) {
+	return func(deck *Deck) {
+		deck.cards.Sort()
+	}
+}
+
+func (deck *Deck) Draw() (*Card, error) {
+	card, err := deck.cards.Delete()
+	if errors.Is(err, sequence.ErrSequenceEmpty) {
+		return nil, ErrDeckEmpty
+	}
+	return card, err
+}
